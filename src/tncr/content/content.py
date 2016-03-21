aed8897ee5423497876ecfba04561b1caa32e9c6
TsungWei Hu <marr.tw@gmail.com>
2016-03-21 15:56:53 +0800

    Update Base Class from Item to Container for Monument Type.

--- src/tncr/content/content.py
+++ src/tncr/content/content.py
@@ -1,11 +1,11 @@
 from zope.interface import implementer
-from plone.dexterity.content import Item
+from plone.dexterity.content import Container
 
 from tncr.content.interfaces import IMonument
 
 
 @implementer(IMonument)
-class Monument(Item):
+class Monument(Container):
     """Item Subclass for Monument
     """
 
