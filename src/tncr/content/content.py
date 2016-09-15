6ce7d28195d43761df702320db0bc6bbbfbbf43f
TsungWei Hu <marr.tw@gmail.com>
2016-09-15 09:16:27 +0800

    Try Adding Image Property But Failed. http://stackoverflow.com/questions/36234373/dexterity-container-type-with-image-attribute

--- src/tncr/content/content.py
+++ src/tncr/content/content.py
@@ -1,11 +1,23 @@
 from zope.interface import implementer
 from plone.dexterity.content import Container
+from Products.CMFCore.utils import getToolByName
 
 from tncr.content.interfaces import IMonument
 
 
 @implementer(IMonument)
 class Monument(Container):
-    """Item Subclass for Monument
+    """Container Subclass for Monument
     """
+
+#   def __init__(self, image=None):
+#       self.image = image
+
+    @property
+    def image(self):
+        catalog = getToolByName(self, 'portal_catalog')
+        path = '/'.join(self.getPhysicalPath())
+        brain = catalog(path={"query": path}, portal_type=['Image'])
+        obj = brain[0].getObject()
+        return obj.image
 
